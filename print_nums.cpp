75a555485a240cffbed46aba6d7f336fac967a79
ahmad-ausaf0 <35466676+ahmad-ausaf0@users.noreply.github.com>
2022-06-03 16:22:13 +0530

    Create print_nums.cpp

---
+++ print_nums.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    int n;
+    std::cout << "Enter a number to print till that number: " <<std::endl;
+    std::cin >> n;
+    int i = 1;
+    while (i <= n) {
+        std::cout << i << std::endl;
+        i = i + 1;
+    }
+
+    return 0;
+}
